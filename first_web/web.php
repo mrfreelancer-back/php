c706d52a7187e5d3e89499d5bb94440463cf54cd
alibarkhordargeek <alibarkhordarbusiness@gmail.com>
2023-04-05 08:08:48 -0700

    first server running

---
+++ first_web/web.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title><?php echo "my website"; ?></title>
+    </head>
+    <body>
+        <h1><?php echo "Hi! here's my first website introduced by mr.php!!!"; ?></h1>
+    </body>
+</html>
